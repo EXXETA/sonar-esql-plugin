b81b09571db8e95a7b48cefca76464acfe298df9
Thomas Pohl <thomas.pohl@exxeta.de>
2017-07-11 16:57:12 +0200

    Improve passthruStatementTest

--- esql-frontend/src/test/java/com/exxeta/iss/sonar/esql/api/tree/impl/statement/PassthruStatementTest.java
+++ esql-frontend/src/test/java/com/exxeta/iss/sonar/esql/api/tree/impl/statement/PassthruStatementTest.java
@@ -18,20 +18,39 @@
 package com.exxeta.iss.sonar.esql.api.tree.impl.statement;
 
 import static com.exxeta.iss.sonar.esql.utils.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 import com.exxeta.iss.sonar.esql.api.tree.Tree.Kind;
+import com.exxeta.iss.sonar.esql.tree.impl.statement.PassthruStatementTreeImpl;
+import com.exxeta.iss.sonar.esql.utils.EsqlTreeModelTest;
 
-public class PassthruStatementTest {
+public class PassthruStatementTest extends EsqlTreeModelTest<PassthruStatementTreeImpl> {
 
 
 	@Test
 	public void passthruStatement(){
 		assertThat(Kind.PASSTHRU_STATEMENT)
-		.matches("PASSTHRU 'CREATE TABLE Shop.Customers (  CustomerNumber INTEGER,  FirstName      VARCHAR(256),  LastName       VARCHAR(256),  Street         VARCHAR(256),  City           VARCHAR(256),  Country        VARCHAR(256))' TO Database.DSN1;");
-
+		.matches("PASSTHRU 'CREATE TABLE Shop.Customers (  CustomerNumber INTEGER,  FirstName      VARCHAR(256),  LastName       VARCHAR(256),  Street         VARCHAR(256),  City           VARCHAR(256),  Country        VARCHAR(256))' TO Database.DSN1;")
+		.matches("PASSTHRU ('', '');")
+		;
 	}
+	
+	@Test
+	public void modelTest() throws Exception{
+		PassthruStatementTreeImpl tree = parse("PASSTHRU '' TO Database.DSN1 VALUES ();", Kind.PASSTHRU_STATEMENT);
+		assertNotNull(tree);
+		assertNotNull(tree.passthruKeyword());
+		assertNotNull(tree.expression());
+		assertNotNull(tree.toKeyword());
+		assertNotNull(tree.databaseReference());
+		assertNotNull(tree.valuesKeyword());
+		assertNotNull(tree.expressionList());
+		assertNotNull(tree.semi());
+	}
+	
+	
 	
 }
  
