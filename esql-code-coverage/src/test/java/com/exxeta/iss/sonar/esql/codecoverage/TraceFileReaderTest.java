e51fc56c6dbc8ff48841beafa2a300a314054930
Thomas Pohl <thomas.pohl@exxeta.de>
2017-09-15 06:34:41 +0200

    Test for xml trace input

--- esql-code-coverage/src/test/java/com/exxeta/iss/sonar/esql/codecoverage/TraceFileReaderTest.java
+++ esql-code-coverage/src/test/java/com/exxeta/iss/sonar/esql/codecoverage/TraceFileReaderTest.java
@@ -1,6 +1,7 @@
 package com.exxeta.iss.sonar.esql.codecoverage;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
@@ -25,12 +26,33 @@ public class TraceFileReaderTest {
 			}
 		};
 		
-		TraceFileReader reader = new TraceFileReader(new File("esql-code-coverage/src/main/resources/MessageRoutingSampleTrace.txt")).readTrace(visitor);
+		TraceFileReader reader = new TraceFileReader(new File("src/test/resources/codecoverage/MessageRoutingSampleTrace.txt")).readTrace(visitor);
 		assertEquals(3, reader.getModuleCount());
 		assertNotNull(reader.getModuleExecutionData(""));
 		assertEquals(2, reader.getModuleExecutionData("").size());
 		assertEquals(3, reader.getModuleExecutionData(".Refresh_memory_cache_Compute").size());
 		assertEquals(36, reader.getModuleExecutionData(".Routing_using_memory_cache_Compute").size());
+		
+	}
+	@Test
+	public void xmlReaderTest() {
+		
+		ExecutionDataVisitor visitor = new ExecutionDataVisitor() {
+			private int moduleCount = 0;
+			@Override
+			public void visitModuleExecution(ModuleExecutionData data) {
+				moduleCount++;
+				assertNotNull(data);
+				assert(moduleCount<=20);
+				assert(data.size()>0);
+				
+			}
+		};
+		
+		TraceFileReader reader = new TraceFileReader(new File("src/test/resources/codecoverage/trace.xml")).readTrace(visitor);
+		assertEquals(1, reader.getModuleCount());
+		assertNull(reader.getModuleExecutionData(""));
+		assertEquals(7, reader.getModuleExecutionData(".Test_Compute").size());
 		
 	}
 	
