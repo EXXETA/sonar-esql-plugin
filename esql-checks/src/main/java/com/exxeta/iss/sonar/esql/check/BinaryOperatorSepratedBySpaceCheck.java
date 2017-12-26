d29b602489642331ea75fcff72582bb337800565
Thomas Pohl <thomas.pohl@exxeta.de>
2017-12-26 09:38:47 +0100

    Let BinaryOperatorSepratedBySpaceCheck use the parsed tree.

--- esql-checks/src/main/java/com/exxeta/iss/sonar/esql/check/BinaryOperatorSepratedBySpaceCheck.java
+++ esql-checks/src/main/java/com/exxeta/iss/sonar/esql/check/BinaryOperatorSepratedBySpaceCheck.java
@@ -3,58 +3,67 @@
  */
 package com.exxeta.iss.sonar.esql.check;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.sonar.check.Rule;
 
-import com.exxeta.iss.sonar.esql.api.tree.ProgramTree;
-import com.exxeta.iss.sonar.esql.api.visitors.DoubleDispatchVisitorCheck;
-import com.exxeta.iss.sonar.esql.api.visitors.EsqlFile;
-import com.exxeta.iss.sonar.esql.api.visitors.LineIssue;
-import com.exxeta.iss.sonar.esql.lexer.EsqlLexer;
+import com.exxeta.iss.sonar.esql.api.tree.Tree;
+import com.exxeta.iss.sonar.esql.api.tree.Tree.Kind;
+import com.exxeta.iss.sonar.esql.api.visitors.SubscriptionVisitorCheck;
+import com.exxeta.iss.sonar.esql.tree.impl.lexical.InternalSyntaxToken;
 import com.google.common.collect.ImmutableList;
 
 /**
- * This Java class is created to implement the logic for all binary operators should be separated from their operands by spaces.
+ * This Java class is created to implement the logic for all binary operators
+ * should be separated from their operands by spaces.
+ * 
  * @author C50679
  *
  */
 @Rule(key = "BinaryOperatorSepratedBySpace")
-public class BinaryOperatorSepratedBySpaceCheck extends DoubleDispatchVisitorCheck{
-	
-	private static final String MESSAGE = "All binary operators should be separated from their operands by spaces.";
-	private static final List<String> BINARY_OPERATOR = ImmutableList.of("=",">","<","=<",">=","<>");
-	 
-	
-	@Override
-	public void visitProgram(ProgramTree tree) {
-		EsqlFile file = getContext().getEsqlFile();
-		List<String>  lines = CheckUtils.readLines(file);
-		int i = 0;
-		
-		for (String line : lines) {
-			i = i + 1;	
-		String upperCaseTheLine = line.toUpperCase().trim();
-		for (String operator : BINARY_OPERATOR) {
-			
-			int size = operator.length();
-			int pos1 = upperCaseTheLine.indexOf(operator);
-			if(upperCaseTheLine.contains(operator) && ( pos1!= -1) && (size==1)){
-				
-				 int pos = upperCaseTheLine.indexOf(operator);
-				 
-				if ((!upperCaseTheLine.substring(pos-1, pos+1).matches(EsqlLexer.WHITESPACE)  && !upperCaseTheLine.substring(pos+1, pos+2).matches(EsqlLexer.WHITESPACE)) 
-						|| (!upperCaseTheLine.substring(pos-1, pos).matches(EsqlLexer.WHITESPACE) && !upperCaseTheLine.substring(pos+2, pos+3).matches(EsqlLexer.WHITESPACE))){
-				addIssue(new LineIssue(this, i, MESSAGE));
-			}
-		  }
-			
-				
-		
-		}
-	}
-  }
+public class BinaryOperatorSepratedBySpaceCheck extends SubscriptionVisitorCheck {
 
-	
-	
+	private static final String MESSAGE = "All binary operators should be separated from their operands by spaces.";
+	private static final List<String> BINARY_OPERATOR = ImmutableList.of("=", ">", "<", "=<", ">=", "<>");
+
+	@Override
+	public void visitNode(Tree tree) {
+		if (BINARY_OPERATOR.contains(((InternalSyntaxToken) tree).text())) {
+			Iterator<Tree> childIterator = tree.parent().childrenStream().iterator();
+			Tree prevChild = null;
+			for (Tree child = childIterator.next(); childIterator.hasNext(); child = childIterator.next()) {
+				if (child == tree) {
+					break;
+				}
+				prevChild = child;
+			}
+			Tree nextChild = null;
+			if (childIterator.hasNext()) {
+				nextChild = childIterator.next();
+			}
+			boolean noSpaceBefore = false;
+			boolean noSpaceAfter = false;
+			if (prevChild!=null && prevChild.lastToken().endLine() == ((InternalSyntaxToken) tree).endLine()) {
+				if (prevChild.lastToken().endColumn() == ((InternalSyntaxToken) tree).column()){
+					noSpaceBefore=true;
+				}
+			}
+			if (nextChild!=null && ((InternalSyntaxToken) tree).endLine() == nextChild.firstToken().line()) {
+				if (((InternalSyntaxToken) tree).endColumn()==nextChild.firstToken().column()){
+					noSpaceAfter = true;
+				}
+			}
+			if (noSpaceAfter || noSpaceBefore){
+				addIssue(tree, MESSAGE);
+			}
+		}
+		super.visitNode(tree);
+	}
+
+	@Override
+	public List<Kind> nodesToVisit() {
+		return ImmutableList.of(Tree.Kind.TOKEN);
+	}
+
 }
--- esql-checks/src/test/java/com/exxeta/iss/sonar/esql/check/BinaryOperatorSepratedBySpaceCheckTest.java
+++ esql-checks/src/test/java/com/exxeta/iss/sonar/esql/check/BinaryOperatorSepratedBySpaceCheckTest.java
@@ -21,7 +21,6 @@ public class BinaryOperatorSepratedBySpaceCheckTest {
 		EsqlCheck check = new BinaryOperatorSepratedBySpaceCheck();
 		EsqlCheckVerifier.issues(check, new File("src/test/resources/BinaryOperatorSpace.esql"))
 		.next().atLine(5).withMessage("All binary operators should be separated from their operands by spaces.")
-		.next().atLine(5).withMessage("All binary operators should be separated from their operands by spaces.")
 		.next().atLine(6).withMessage("All binary operators should be separated from their operands by spaces.")
 		.next().atLine(7).withMessage("All binary operators should be separated from their operands by spaces.")
 		.next().atLine(8).withMessage("All binary operators should be separated from their operands by spaces.")
