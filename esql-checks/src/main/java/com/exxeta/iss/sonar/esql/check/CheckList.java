8e645421b993e56930bd13761a8e126ac81943ea
Thomas Pohl <thomas.pohl@exxeta.de>
2018-01-24 07:27:31 +0100

    Remove FileHeaderCommentCheck. It is replaced by FileHeaderCheck. #25

--- esql-checks/src/main/java/com/exxeta/iss/sonar/esql/check/CheckList.java
+++ esql-checks/src/main/java/com/exxeta/iss/sonar/esql/check/CheckList.java
@@ -99,7 +99,6 @@ public final class CheckList {
         InsertBlankLineBetweenFuncProcCheck.class,
         BlankLineBeforeCommentsCheck.class,
         RoutineCommentsCheck.class,
-        FileHeaderCommentsCheck.class,
 		PropagateConsistencyCheck.class, 
 		UnreachableCodeCheck.class, 
 		CommentedCodeCheck.class,
