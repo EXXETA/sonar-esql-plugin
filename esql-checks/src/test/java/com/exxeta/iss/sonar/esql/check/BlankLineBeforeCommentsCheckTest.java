be781308cd932b9ec50411618951c542aa52f0a8
Thomas Pohl <thomas.pohl@exxeta.de>
2018-02-14 09:02:34 +0100

    Fixes #70 - @rudrakshi thanks for reporting

--- esql-checks/src/test/java/com/exxeta/iss/sonar/esql/check/BlankLineBeforeCommentsCheckTest.java
+++ esql-checks/src/test/java/com/exxeta/iss/sonar/esql/check/BlankLineBeforeCommentsCheckTest.java
@@ -33,8 +33,8 @@ public class BlankLineBeforeCommentsCheckTest {
 	public void test() {
 		EsqlCheck check = new BlankLineBeforeCommentsCheck();
 		EsqlCheckVerifier.issues(check, new File("src/test/resources/BlankLineBeforeComments.esql"))
-		.next().atLine(4).withMessage("Insert one blank line before a block or single-line comment.")
-		.next().atLine(16).withMessage("Insert one blank line before a block or single-line comment.")
+		.next().atLine(4).withMessage("Insert one blank line before this block or single-line comment.")
+		.next().atLine(16).withMessage("Insert one blank line before this block or single-line comment.")
 		.noMore();
 	}
 
