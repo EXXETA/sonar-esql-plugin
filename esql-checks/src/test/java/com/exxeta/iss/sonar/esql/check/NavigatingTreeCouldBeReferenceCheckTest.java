df76f76192b992bda85258e4b9525f07e911cace
Thomas Pohl <thomas.pohl@exxeta.com>
2020-08-02 08:35:50 +0200

    Improve NavigatingTreeCouldBeReferenceCheck #119

--- esql-checks/src/test/java/com/exxeta/iss/sonar/esql/check/NavigatingTreeCouldBeReferenceCheckTest.java
+++ esql-checks/src/test/java/com/exxeta/iss/sonar/esql/check/NavigatingTreeCouldBeReferenceCheckTest.java
@@ -6,9 +6,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,28 +16,37 @@
  * limitations under the License.
  */
 package com.exxeta.iss.sonar.esql.check;
-import java.io.File;
 
+import com.exxeta.iss.sonar.esql.checks.verifier.EsqlCheckVerifier;
 import org.junit.Test;
 
-import com.exxeta.iss.sonar.esql.api.EsqlCheck;
-import com.exxeta.iss.sonar.esql.checks.verifier.EsqlCheckVerifier;
+import java.io.File;
+
 /**
- * This java  class is  the test class for NavigatingTreeCouldBeReferenceCheck.java. 
- * @author Sapna. singh
+ * This java  class is  the test class for NavigatingTreeCouldBeReferenceCheck.java.
  *
+ * @author Sapna. singh
  */
 public class NavigatingTreeCouldBeReferenceCheckTest {
 
-	
-     @Test
-		public void test() {
-			EsqlCheck check = new NavigatingTreeCouldBeReferenceCheck();
-			EsqlCheckVerifier.issues(check, new File("src/test/resources/NavigatingTreeCouldBeReference.esql"))
-					.next().atLine(4).withMessage("Navigating message tree could be replaced by a reference.")
-					.next().atLine(16).withMessage("Navigating message tree could be replaced by a reference.")
-					.next().atLine(16).withMessage("Navigating message tree could be replaced by a reference.")
-			.noMore();
-		}
+
+    @Test
+    public void test() {
+        NavigatingTreeCouldBeReferenceCheck check = new NavigatingTreeCouldBeReferenceCheck();
+        EsqlCheckVerifier.issues(check, new File("src/test/resources/NavigatingTreeCouldBeReference.esql"))
+                .next().atLine(4).withMessage("Navigating message tree could be replaced by a reference.")
+                .next().atLine(16).withMessage("Navigating message tree could be replaced by a reference.")
+                .next().atLine(16).withMessage("Navigating message tree could be replaced by a reference.")
+                .noMore();
+        check.maximumAllowedPathElements = 3;
+        EsqlCheckVerifier.issues(check, new File("src/test/resources/NavigatingTreeCouldBeReference.esql"))
+                .next().atLine(4).withMessage("Navigating message tree could be replaced by a reference.")
+                .noMore();
+		check.maximumAllowedPathElements = 2;
+        check.allowedNumberOfFieldReferenceUses = 4;
+        EsqlCheckVerifier.issues(check, new File("src/test/resources/NavigatingTreeCouldBeReference.esql"))
+                .next().atLine(16).withMessage("Navigating message tree could be replaced by a reference.")
+                .noMore();
+    }
 }
 
